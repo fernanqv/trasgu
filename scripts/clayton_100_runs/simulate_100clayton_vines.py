3a90f1c29f31c3697d104cb0e5fa4c961ea9115b
Valvanuz Fernandez <valvanuz.fernandez@unican.es>
2026-07-14 14:00:33 +0200

    Updated config: 7 vars fit finished in altamira

--- scripts/clayton_100_runs/simulate_100clayton_vines.py
+++ scripts/clayton_100_runs/simulate_100clayton_vines.py
@@ -9,18 +9,19 @@ TEMPLATE_CONFIG = BASE_DIR / "trasgu.yaml"
 SIMULATIONS_DIR = BASE_DIR / "simulations"
 N_ITERATIONS = 100
 
-# Matrix ID: ****
-matrix = np.array(
-    [
-        [7, 1, 6, 3, 4, 6, 6],
-        [3, 6, 4, 4, 6, 4, 0],
-        [4, 4, 3, 6, 3, 0, 0],
-        [6, 3, 7, 7, 0, 0, 0],
-        [1, 7, 1, 0, 0, 0, 0],
-        [2, 2, 0, 0, 0, 0, 0],
-        [5, 0, 0, 0, 0, 0, 0],
+matrix_inv= np.array(
+    [ [ 7, 7, 3, 4, 4, 7, 6 ],
+      [ 0, 3, 7, 3, 3, 3, 4 ],
+      [ 0, 0, 4, 7, 2, 4, 3 ],
+      [ 0, 0, 0, 2, 7, 2, 2 ],
+      [ 0, 0, 0, 0, 6, 6, 7 ],
+      [ 0, 0, 0, 0, 0, 5, 5 ],
+      [ 0, 0, 0, 0, 0, 0, 1 ]
     ]
 )
+
+matrix = matrix_inv[:, ::-1]
+print(matrix)
 
 bicop = pv.Bicop(pv.clayton, parameters=np.array([[6.8]]))
 pair_copulas = [
